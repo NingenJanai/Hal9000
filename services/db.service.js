08b8837b0d369b59c4a8ca8736cb86e702ac49c4
NingenJanai <onishirox@gmail.com>
2020-10-20 16:08:19 +0200

    Update db.service.js

--- services/db.service.js
+++ services/db.service.js
@@ -2,25 +2,117 @@ var winston = require('winston');
 var moment = require('moment');
 
 const _ = require('lodash');
-const monk = require('monk');
+const MongoClient = require('mongodb').MongoClient;
 const { Observable, Observer } = require('rxjs');
 
 module.exports = class DBService {
     constructor(MONGO_DB) {
         this.MONGO_DB = MONGO_DB;
     }
-
     createTriviaCategories() {
         return Observable.create(observer => {
             try {
                 let categories = require('../trivia.categories.json');
 
-                let db = monk(this.MONGO_DB);
+                MongoClient.connect(this.MONGO_DB, function (err, db) {
+                    if (err) throw err;
+                    var dbo = db.db("hal");
+                    let collection = dbo.collection("trivia_categories");
+                    collection.deleteMany({})
+                        .then(() => {
+                            return collection.insertMany(categories);
+                        })
+                        .then(docs => {
+                            observer.next(docs);
+                            observer.complete();
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            winston.error(err);
+                            observer.error(err);
+                        })
+                        .then(() => db.close());
+                });
+            } catch (err) {
+                console.log(err);
+                winston.error(err);
+                observer.error(err);
+            }
+        });
+    }
 
-                let collection = db.get('trivia_categories');
-                collection.remove({})
+    getTriviaCategories() {
+        return Observable.create(observer => {
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                dbo.collection("trivia_categories").find({}).toArray(function (err, docs) {
+                    if (err) throw err;
+                    console.log('trivia_categories', docs);
+                    observer.next(docs);
+                    observer.complete();
+                    db.close();
+                });
+            });
+        });
+    }
+
+    createCommands() {
+        return Observable.create(observer => {
+            try {
+                let commands = require('../commands.json');
+
+                MongoClient.connect(this.MONGO_DB, function (err, db) {
+                    if (err) throw err;
+                    var dbo = db.db("hal");
+                    let collection = dbo.collection("commands");
+                    collection.deleteMany({})
+                        .then(() => {
+                            return collection.insertMany(commands);
+                        })
+                        .then(docs => {
+                            observer.next(docs);
+                            observer.complete();
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            winston.error(err);
+                            observer.error(err);
+                        })
+                        .then(() => db.close());
+                });
+            } catch (err) {
+                console.log(err);
+                winston.error(err);
+                observer.error(err);
+            }
+        });
+    }
+
+    getCommands() {
+        return Observable.create(observer => {
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                dbo.collection("commands").find({}).toArray(function (err, docs) {
+                    if (err) throw err;
+                    observer.next(docs);
+                    observer.complete();
+                    db.close();
+                });
+            });
+        });
+    }
+
+    saveTournament(tournament) {
+        return Observable.create(observer => {
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                let collection = dbo.collection("tournaments");
+                collection.deleteMany({})
                     .then(() => {
-                        return collection.insert(categories);
+                        return collection.insertOne({ timestamp: Date.now(), category: tournament.getCategoryID(), users: [] });
                     })
                     .then(docs => {
                         observer.next(docs);
@@ -32,185 +124,101 @@ module.exports = class DBService {
                         observer.error(err);
                     })
                     .then(() => db.close());
-
-            } catch (err) {
-                console.log(err);
-                winston.error(err);
-                observer.error(err);
-            }
-        });
-    }
-
-    getTriviaCategories() {
-        return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('trivia_categories');
-
-            collection.find()
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
-        });
-    }
-
-    createCommands() {
-        return Observable.create(observer => {
-            try {
-                let commands = require('../commands.json');
-
-                let db = monk(this.MONGO_DB);
-
-                let collection = db.get('commands');
-                collection.remove({})
-                    .then(() => {
-                        return collection.insert(commands);
-                    })
-                    .then(docs => {
-                        observer.next(docs);
-                        observer.complete();
-                    })
-                    .catch(err => {
-                        winston.error(err);
-                        observer.error(err);
-                    })
-                    .then(() => db.close());
-
-            } catch (err) {
-                winston.error(err);
-                observer.error(err);
-            }
-        });
-    }
-
-    getCommands() {
-        return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('commands');
-
-            collection.find()
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
-        });
-    }
-
-    saveTournament(tournament) {
-        return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('tournaments');
-
-            collection.insert({ timestamp: Date.now(), category: tournament.getCategoryID(), users: [] })
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
+            });
         });
     }
 
     deleteTournament(tournamentID) {
         return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('tournaments');
-            collection.remove({ _id: tournamentID })
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                let collection = dbo.collection("tournaments");
+                collection.deleteOne({ _id: tournamentID })
+                    .then(() => {
+                        observer.next(true);
+                        observer.complete();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        winston.error(err);
+                        observer.error(false);
+                    })
+                    .then(() => db.close());
+            });
         });
     }
 
     saveTournamentUser(tournamentID, userID) {
         return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('tournaments');
-
-            collection.update(
-                { _id: tournamentID },
-                {
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                let collection = dbo.collection("tournaments");
+                collection.updateOne({ _id: tournamentID }, {
                     $push: {
                         "users": { userID: userID, timestamp: Date.now() }
                     }
                 })
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
+                    .then(() => {
+                        observer.next(true);
+                        observer.complete();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        winston.error(err);
+                        observer.error(false);
+                    })
+                    .then(() => db.close());
+            });
         });
     }
 
     saveQuestion(question) {
         return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('questions');
-
-            collection.insert({ timestamp: Date.now(), category: question.getCategoryID(), tournament_id: question.getTournamentID(), users: [] })
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                let collection = dbo.collection("questions");
+                collection.insertOne({ timestamp: Date.now(), category: question.getCategoryID(), tournament_id: question.getTournamentID(), users: [] })
+                    .then((docs) => {
+                        observer.next(docs);
+                        observer.complete();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        winston.error(err);
+                        observer.error(false);
+                    })
+                    .then(() => db.close());
+            });
         });
     }
 
     saveAnswer(questionID, userID, correct) {
         return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('questions');
-
-            collection.update(
-                { _id: questionID },
-                {
-                    $push: {
-                        "users": { userID: userID, correct: correct, timestamp: Date.now() }
-                    }
-                })
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                let collection = dbo.collection("questions");
+                collection.updateOne(
+                    { _id: questionID },
+                    {
+                        $push: {
+                            "users": { userID: userID, correct: correct, timestamp: Date.now() }
+                        }
+                    })
+                    .then((docs) => {
+                        observer.next(docs);
+                        observer.complete();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        winston.error(err);
+                        observer.error(false);
+                    })
+                    .then(() => db.close());
+            });
         });
     }
 
@@ -223,11 +231,8 @@ module.exports = class DBService {
 
     getRankingData(start, end) {
         return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('questions');
-
             var operations = [];
+
             if (start && end) {
                 operations.push({
                     $match: {
@@ -235,7 +240,6 @@ module.exports = class DBService {
                     }
                 });
             }
-
             operations.push({
                 $unwind: {
                     path: "$users",
@@ -267,71 +271,79 @@ module.exports = class DBService {
                 }
             });
 
-            collection.aggregate(operations)
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                let collection = dbo.collection("questions");
+                collection.aggregate(operations)
+                    .then((docs) => {
+                        observer.next(docs);
+                        observer.complete();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        winston.error(err);
+                        observer.error(false);
+                    })
+                    .then(() => db.close());
+            });
         });
     }
 
     getTournamentRankingData(tournament_id) {
         return Observable.create(observer => {
-            let db = monk(this.MONGO_DB);
-
-            let collection = db.get('questions');
-
-            collection.aggregate([
-                {
-                    $match: {
-                        tournament_id: tournament_id
-                    }
-                },
-                {
-                    $unwind: {
-                        path: "$users",
-                        preserveNullAndEmptyArrays: true
-                    }
-                },
-                {
-                    $project: {
-                        "users.userID": 1,
-                        "users.correct": 1
-                    }
-                },
-                {
-                    $match: {
-                        "users.correct": true
-                    }
-                },
-                {
-                    $group: {
-                        _id: "$users.userID",
-                        points: {
-                            $sum: 1
+            MongoClient.connect(this.MONGO_DB, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("hal");
+                let collection = dbo.collection("questions");
+                collection.aggregate([
+                    {
+                        $match: {
+                            tournament_id: tournament_id
+                        }
+                    },
+                    {
+                        $unwind: {
+                            path: "$users",
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    {
+                        $project: {
+                            "users.userID": 1,
+                            "users.correct": 1
+                        }
+                    },
+                    {
+                        $match: {
+                            "users.correct": true
+                        }
+                    },
+                    {
+                        $group: {
+                            _id: "$users.userID",
+                            points: {
+                                $sum: 1
+                            }
+                        }
+                    },
+                    {
+                        $sort: {
+                            points: -1
                         }
                     }
-                },
-                {
-                    $sort: {
-                        points: -1
-                    }
-                }
-            ])
-                .then(docs => {
-                    observer.next(docs);
-                    observer.complete();
-                })
-                .catch(err => {
-                    winston.error(err);
-                    observer.error(err);
-                })
-                .then(() => db.close());
+                ])
+                    .then((docs) => {
+                        observer.next(docs);
+                        observer.complete();
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        winston.error(err);
+                        observer.error(false);
+                    })
+                    .then(() => db.close());
+            });
         });
     }
 }
